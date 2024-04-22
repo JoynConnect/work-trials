4599c24d9e6be5c110b622d6cc9b663d91cc248c
Rebecca Lovering <lovering810@gmail.com>
2024-04-22 11:57:01 -0400

    modify tests for new class

--- JD-Lead Backend-Engineer-(Data)/joynBEE/tests/test_lang.py
+++ JD-Lead Backend-Engineer-(Data)/joynBEE/tests/test_lang.py
@@ -3,15 +3,22 @@ import joynBEE.langtools as lt
 
 from joynBEE.basic import assemble_corpus
 
+all_platforms = ["jira", "notion", "slack"]
+
+CORPUS = assemble_corpus(all_platforms)
+
 
 @pytest.fixture
 def nlp():
-    all_platforms = ["jira", "notion", "slack"]
-
-    corpus = assemble_corpus(all_platforms)
     # Chose Cohere as the API that supported RAG and
     # didn't give me rate-limit issues.
-    return lt.EmbeddedCorpus(corpus=corpus, tool="cohere")
+    return lt.EmbeddedCorpus(corpus=CORPUS, tool="cohere")
+
+
+@pytest.fixture
+def ragdoll():
+
+    return lt.RAGDoll(corpus=CORPUS, tool="cohere")
 
 
 def test_init(nlp):
@@ -19,24 +26,22 @@ def test_init(nlp):
     assert nlp
     assert nlp.llm
     assert nlp.embeddings
-    assert nlp.retrievador
     assert nlp.logger
 
     with pytest.raises(NotImplementedError):
         _ = lt.EmbeddedCorpus(corpus=["x", "y", "z"], tool="fake")
 
 
-def test_query(nlp):
+def test_query(ragdoll):
 
-    res = nlp.query("Quis custodiet ipsos custodies?")
-    assert isinstance(res, dict)
-    assert len(res["context"]) == 4
+    res = ragdoll.query("Quis custodiet ipsos custodies?")
+    assert isinstance(res, str)
 
 
-def test_authorship(nlp):
+# def test_authorship(ragdoll):
 
-    res = nlp.query(
-        "Which slack user writes most like jira user 'Jimmie82@hotmail.com'?"
-    )
-    assert isinstance(res, dict)
-    assert len(res["context"]) == 4
+#     res = nlp.query(
+#         "Which slack user writes most like jira user 'Jimmie82@hotmail.com'?"
+#     )
+#     assert isinstance(res, dict)
+#     assert len(res["context"]) == 4
